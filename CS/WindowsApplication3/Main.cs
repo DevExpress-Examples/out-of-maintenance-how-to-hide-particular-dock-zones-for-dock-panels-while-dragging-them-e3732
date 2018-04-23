f78503ff7673068603dd9394ba8df3ea2c1b392b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WindowsApplication3/Main.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+
+namespace WindowsApplication3 {
+    public partial class Main: XtraForm {
+        public Main() {
+            InitializeComponent();
+        }
+    }
+}
